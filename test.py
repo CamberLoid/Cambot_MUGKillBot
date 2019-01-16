a1dddbf23b113f88e07f629d5d5d1dd61a7080a8
Billy Huang <mrsc4@icloud.com>
2019-01-16 14:16:10 +0800

    Null

--- test.py
+++ test.py
@@ -1,13 +1,25 @@
 import requests,requests_oauthlib
 import asyncio,sys,os
 from pprint import pprint
+from base64 import b64encode,b64decode
 import json
+
+arcauth = "https://arcapi.lowiro.com/4/auth/login"
 arcapi = "https://arcapi.lowiro.com/4/user/me"
+def getAuth(cred,pswd):
+    toencode = cred+":"+pswd
+    headers = {
+          'Authorization': 'Basic ' + b64encode(
+              toencode.encode('ascii')).decode(),
+          'DeviceId': 'web',
+          'AppVersion': 'web'
+        }
+    po = requests.post(arcauth,headers=headers)
+    return po
 headers = {}
 Bearer = sys.argv[1]
 headers["authorization"] = "Bearer "+"/6wiU25MYuHR1NvI0wlsq+Ufky13W2C0fh2mblBu7pQ="
-
 r=requests.get(arcapi,headers=headers)
-try:
-    r.raise_for_status()
-    attr=r.json()
+r.raise_for_status()
+attr=r.json()
+
