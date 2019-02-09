05e5b34baf31700aa1ad8aac90d2ae63fea1c379
Camberloid <camberloid@outlook.com>
2019-02-09 13:15:10 +0800

    0x04 Release

--- mainbot.py
+++ mainbot.py
@@ -6,14 +6,18 @@
 
 import telepot,telepot.aio
 from telepot.aio.loop import MessageLoop
-import os,sys,time,json,logging,asyncio,traceback
-import Modules.arcaea as arc
+import os,sys,time,json,logging,asyncio,traceback,requests.exceptions
+# import Modules.arcaea as arc
+from Modules import Hikari
 from botCore import jsonHandle
 from pprint import pprint
 
+logging.info("Precheck By /user/me") 
+IArc = Hikari.api.Arcaea()
+logging.info("Precheck Complete, Initalizing")
+
 atBot = "@HikariissoKawaiiBot"
-arc_pool = {}
-async def register(msg ,*args, **kwargs):
+'''async def register(msg ,*args, **kwargs):
     chatid,msgid,usrid,command = \
     msg['chat']['id'],msg['message_id'],msg['from']['id'],msg['text'].split(' ')
     username = msg['from'].get('username')
@@ -94,19 +98,70 @@ async def showRecent(msg, *args, **kwargs): #show
         pass
     elif len(command) == 1: reply =  d.getRecent(msg['from']['first_name']+' / ')       
     elif len(command) == 2: reply = d.getFriendRecent(command[1])
-    return reply
+    return reply'''
 def about():
-    return """这里是 光酱超可爱 Bot(请不要吐槽头像) 基于MIT协议开源
-项目源代码 at https://github.com/CamberLoid/Cambot_MUGKillBot/tree/simple_release, 欢迎各路大佬贡献代码
+    return """Hallo 这里是 Hikari v2
+项目源代码 at https://github.com/CamberLoid/Cambot_MUGKillBot/tree/Hikari_v2, 欢迎各路大佬贡献代码
+把api wrapper重写了之后重新上线, 目前只开放用friend code查询
+功能将逐步完善(*咕)
 
-目前处于收集异常数据情况,用户数据已清空(1月20/00:38)
-Bot计划运行在阿里云香港的轻量机上面
+Bot还是跑在自己电脑上:P
 指令请见下方 [/]
 """
 def start():
     reply = about()
     return reply
-        
+
+def timeGen(unixtime):
+    import time
+    while unixtime > 10000000000: unixtime /= 10
+    timestu = time.gmtime(unixtime)
+    return 'UTC {}-{}-{} {}:{}:{}'.format(
+        timestu.tm_year,timestu.tm_mon,timestu.tm_mday,
+        timestu.tm_hour,timestu.tm_min,timestu.tm_sec)
+
+async def showRecent(msg):
+    chatid,msgid,usrid,command = \
+    msg['chat']['id'],msg['message_id'],msg['from']['id'],msg['text'].split(' ')
+    # username = msg['from'].get('username',msg['from']['first_name'])
+    if len(command) != 2: return
+    friendid = None
+    try:
+        friend_code = command[1]
+        friend = IArc.addFriend(friend_code)
+        friendid = friend['user_id']
+        try: 
+            recent = friend['recent_score'][0]
+            # 处理时间
+            time_played = recent['time_played']/1000
+            
+            reply = '''玩家 {} / Friend Code:{} 
+PTT: {}
+最近的游玩记录:\n
+{}-{} {}
+@{}
+Pure {}({})
+Far  {}
+Miss {}'''.format(
+                friend['name'],friend['user_code'],
+                friend['rating']/100,
+                Hikari.difficulties[recent['difficulty']],Hikari.song_id.get(recent['song_id'],recent['song_id']),Hikari.clear_type[recent['clear_type']],
+                timeGen(recent['time_played']),
+                recent['perfect_count'],recent['shiny_perfect_count'],recent['near_count'],recent['miss_count'])
+        except IndexError: reply = '玩家 {} / Friend Code:{} \n\n还没有在线游玩记录'
+        await asyncio.sleep(1)
+        IArc.delFriend(friendid)
+    except Hikari.exceptions.FriendError as err:
+        traceback.print_exc()
+        reply = '非常抱歉，Bot脚本抛出了一个异常\n一般是找不到用户之类或者已添加之类的\n以及目前还没将errcode分类'
+    except:
+        try: 
+            if friendid != None: IArc.delFriend(friendid)
+        except: pass
+        return traceback.format_exc()
+    return reply
+async def delFriend(msg):
+    pass
 async def msgHandler(msg):
     print(msg['text'])
     reply = None
@@ -121,13 +176,13 @@ async def msgHandler(msg):
         if command[0] != '/': return #非指令退出
         Command = command.split(' ')
         if Command[0][-len(atBot):] == atBot: Command[0] = Command[0][:-len(atBot)] 
-        pprint(Command)
+        pprint(Command) # -> logging
         if Command[0] == '/start': reply = start()
-        if Command[0] == '/register': reply = await register(msg,command=Command)
+        # if Command[0] == '/register': reply = await register(msg,command=Command)
         if Command[0] == '/show': reply = await showRecent(msg)
-        if Command[0] == '/help': reply = '暂时没有帮助信息哦'+start()
+        if Command[0] == '/help': reply = '暂时没有帮助信息哦\n\n'+start()
         print(reply+'\n'+msg['from']['first_name'])
-    except: return "不管怎样反正出错了\n{}".format(traceback.format_exc())
+    except: reply = "不管怎样反正出错了\n{}".format(traceback.format_exc())
     traceback.print_exc()
     if reply != None:
         await Bot.sendMessage(text=reply,chat_id=chatid,reply_to_message_id=msgid)
